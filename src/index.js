26241bd06942f0858a6c3e4682c6bd10305d5a5a
Geice Sousa Pinho <geicesousa@outlook.com>
2023-01-16 13:46:05 -0300

    atualização dos arqvs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+// criar constantes com os inputs
+// usar o .value para acessar o numero digitado
+
+
+const form = document.getElementsByClassName("card-infos");
+const formulario = form.value;
+console.log(formulario);
+
+const numeroCartao = document.querySelector(".card-numero").value;
+console.log(numeroCartao);
+
+const validade = document.querySelector(".card-validade").value;
+console.log(validade);
+
